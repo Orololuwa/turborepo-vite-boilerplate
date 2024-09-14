80333ca2e099654cb3e7291c327b8ca175977a2d
orololuwa <emmybest.ea@gmail.com>
2024-09-14 22:37:39 +0100

    feat: add chakra-ui to ui packages

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,6 +1,10 @@
+export { ChakraProvider } from "@chakra-ui/react";
+
 // utils
 export { setupCounter } from "./utils/counter";
 
 // components
 export { Header } from "./components/header";
 export { Counter } from "./components/counter";
+
+export * from "./components/nav";
